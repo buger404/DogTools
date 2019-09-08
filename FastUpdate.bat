816b8701a51adfba59f8411e7b8642cc6c4ea8ea
ris_vb@126.com <ris_vb@126.com>
2019-09-08 16:07:26 +0800

    first commit

--- FastUpdate.bat
+++ FastUpdate.bat
@@ -2,7 +2,7 @@
 set /p url="Your repo name:"
 git init
 git remote add origin git@github.com:buger404/%url%.git
-git remote set-url --add gitee git@gitee.com:buger404/%url%.git
+git remote set-url --add origin git@gitee.com:buger404/%url%.git
 git add .
 git commit -m "first commit"
 git push -u origin master
